ebd3c503fff89d6ba672f4a46aa6c9d290b65986
Hendrick <sebastinesoacatp@gmail.com>
2021-02-27 05:13:00 +0100

    Writing a composable counter, State hoisting

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -36,12 +36,10 @@ fun MyApp(content: @Composable () -> Unit){
 }
 
 @Composable
-fun Counter(){
+fun Counter(count: Int, updateCount: (Int) -> Unit){
 
-    val count = remember { mutableStateOf(0) }
-
-    Button(onClick = { count.value++ }) {
-        Text("I've been clicked ${count.value} times")
+    Button(onClick = { updateCount(count+1) }) {
+        Text("I've been clicked $count times")
     }
 }
 
@@ -53,13 +51,20 @@ fun Greeting(name: String) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
         Divider(color = Color.Transparent, thickness = 32.dp)
-        Counter()
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
     }
 }
 
